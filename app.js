56cecb400bbb7137355c0ae62a95d31145a1a55d
Sofia.Ferragut <sofia.ferragut@gmail.com>
2019-09-05 22:35:40 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,98 @@
+new Vue({
+    el: '#app',
+    data: {
+        playerHealth: 100,
+        monsterHealth: 100,
+        gameIsRunning: false,
+        
+        turns: []
+    
+    },
+    methods: {
+        startGame: function () {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+        },
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'El jugador golpea al monstruo por ' + damage
+            });                  
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+        },
+
+        specialAttack: function () {
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'El jugador golpea duramente al monstruo por ' + damage
+            });
+            if (this.checkWin()) {
+                return;
+            }
+            this.monsterAttacks();
+
+        },
+
+        heal: function () {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'El jugador se cura en ' + 10
+            });
+            this.monsterAttacks();
+        },
+        
+        giveUp: function () {
+              this.gameIsRunning = false;
+        },
+       
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'El monstruo lastima al jugador en ' + damage
+            });
+            this.checkWin();          
+        },
+
+        calculateDamage: function(min, max) {
+
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        
+        },
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm('Ganaste! Jugar de nuevo?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('Perdiste! Jugar de nuevo?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+       
+    }
+});
